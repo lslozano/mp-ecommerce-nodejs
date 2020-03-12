c06b1b0f3e800ab0f5387a9f15d7eb3c06cdd300
Luis Lozano <luislozano@MacBook-Air.local>
2020-03-11 22:01:53 -0600

    Modified files to implement mercado pago

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+
+const mercadopago = require ('mercadopago');
+
+mercadopago.configure({
+    sandbox: true,
+    access_token: ''
+});
+
+   
 var app = express();
  
 app.engine('handlebars', exphbs());
@@ -11,7 +19,24 @@ app.get('/', function (req, res) {
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+    const { img, title, price, unit } = req.query
+    let preference = {
+      items: [
+        {
+          "picture_url": img,
+          "title": title,
+          "unit_price": Number(price),
+          "quantity": Number(unit)
+        }
+      ]
+    };
+    mercadopago.preferences.create(preference)
+    .then(function(response){
+       global.init_point = response.body.init_point;
+       res.render('detail', {img, title, price, unit, id: response.body.id})
+    }).catch(function(error){
+      console.log(error);
+    });
 });
 
 app.use(express.static('assets'));
